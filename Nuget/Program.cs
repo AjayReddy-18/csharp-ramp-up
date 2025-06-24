4376fe9c4c39fdaa824067ffde27fad54d00b73a
Ajay Reddy <137703598+AjayReddy-18@users.noreply.github.com>
2025-06-24 15:32:25 +0530

    Creates a Project using dotnet cli and adds Newtonsoft.Json package in it and uses it

---
+++ Nuget/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json; 
+
+namespace Nuget
+{
+    public class Account
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime DOB { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account
+            {
+                Name = "Ajay Reddy",
+                Email = "ajay.reddy",
+                DOB = new DateTime(2006, 5, 18, 0, 0, 0, DateTimeKind.Utc),
+            };
+
+            string json = JsonConvert.SerializeObject(account, Formatting.Indented);
+            Console.WriteLine(json);
+            Console.ReadLine();
+        }
+    }
+}
