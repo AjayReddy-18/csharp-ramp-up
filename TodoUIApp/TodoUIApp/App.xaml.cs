02b4160085a6e76ec2834772e6b11a0daaad9bca
Ajay Reddy <137703598+AjayReddy-18@users.noreply.github.com>
2025-07-07 11:17:41 +0530

    creates UI version of Todo App using WPF

---
+++ TodoUIApp/TodoUIApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TodoUIApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ TodoUIApp/TodoUIApp/Task.cs
@@ -0,0 +1,14 @@
+﻿namespace TodoApp
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+        public string Title { get; set; }
+        public int Done { get; set; }
+        public bool IsCompleted
+        {
+            get => Done == 1;
+            set => Done = value ? 1 : 0;
+        }
+    }
+};
