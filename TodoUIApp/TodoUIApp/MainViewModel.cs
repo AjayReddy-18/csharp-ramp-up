d80a98b8df622be8eb33d4a9714b6fb893ae37a2
Ajay Reddy <137703598+AjayReddy-18@users.noreply.github.com>
2025-07-07 17:14:01 +0530

    implements MVVM architecture

---
+++ TodoUIApp/TodoUIApp/MainViewModel.cs
@@ -0,0 +1,71 @@
+﻿namespace TodoUIApp;
+
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data.SQLite;
+using System.Windows.Input;
+using TodoApp;
+using Task = TodoApp.Task;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+    public ObservableCollection<Task> Tasks { get; } = new ObservableCollection<Task>();
+
+    private readonly Todo _todo;
+    private string _newTaskText;
+
+    public string NewTaskText
+    {
+        get => _newTaskText;
+        set
+        {
+            Console.WriteLine(value);
+            if (_newTaskText != value)
+            {
+                _newTaskText = value;
+                OnPropertyChanged(nameof(NewTaskText));
+                ((RelayCommand)AddTaskCommand).RaiseCanExecuteChanged();
+            }
+        }
+    }
+
+    public ICommand AddTaskCommand { get; }
+
+    public MainViewModel()
+    {
+        var dbPath = @"Data Source=C:\Users\fws-t\workspace\personal\cs\TodoUIApp\TodoUIApp\todo.db";
+        var connection = new SQLiteConnection(dbPath);
+        _todo = new Todo(connection);
+
+        AddTaskCommand = new RelayCommand(AddTask, CanAddTask);
+
+        LoadTasks();
+    }
+
+    private void LoadTasks()
+    {
+        Tasks.Clear();
+        foreach (var task in _todo.GetAllTasks())
+        {
+            Tasks.Add(task);
+        }
+    }
+
+    private void AddTask()
+    {
+        _todo.AddTask(NewTaskText);
+        NewTaskText = string.Empty;
+        LoadTasks();
+    }
+
+    private bool CanAddTask()
+    {
+        Console.WriteLine("In can add task");
+        return !string.IsNullOrWhiteSpace(NewTaskText);
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged(string propertyName) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
--- TodoUIApp/TodoUIApp/MainWindow.xaml.cs
+++ TodoUIApp/TodoUIApp/MainWindow.xaml.cs
@@ -1,45 +1,11 @@
-﻿namespace TodoUIApp;
-
-using System.Collections.ObjectModel;
-using System.Data.SQLite;
-using System.Windows;
-using TodoApp;
-using Task = TodoApp.Task;
+﻿using System.Windows;
+namespace TodoUIApp;
 
 public partial class MainWindow : Window
 {
-    public ObservableCollection<Task> Tasks { get; set; } = new ObservableCollection<Task>();
-
-    private readonly Todo _todo;
-
     public MainWindow()
     {
         InitializeComponent();
-        DataContext = this;
-
-        var dbPath = @"Data Source=C:\Users\fws-t\workspace\personal\cs\TodoUIApp\TodoUIApp\todo.db";
-        var connection = new SQLiteConnection(dbPath);
-        _todo = new Todo(connection);
-
-        LoadTasks();
-    }
-
-    private void LoadTasks()
-    {
-        Tasks.Clear();
-        foreach (var task in _todo.GetAllTasks())
-        {
-            Tasks.Add(task);
-        }
-    }
-
-    private void AddTask_Click(object sender, RoutedEventArgs e)
-    {
-        if (!string.IsNullOrWhiteSpace(TaskInput.Text))
-        {
-            _todo.AddTask(TaskInput.Text);
-            TaskInput.Clear();
-            LoadTasks();
-        }
+        DataContext = new MainViewModel();
     }
 }
